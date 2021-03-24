8b07b7d6ae13c1744052a6554ac3dcb15092fd7a
Sergey Kornienko <skornienko29@gmail.com>
2021-03-24 21:34:18 +0300

    with poetry

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kr6%+t@j90ucq-vg%j3quvp40=%kze_i7u@0c6_pnghc4a^+o5'
+SECRET_KEY = {{ SECRET_KEY }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
